0a7f3c88b64dff244a16e5a96c951ccd70c8a0d8
Saad Sayyed <saadsyed950@gmail.com>
2026-04-25 18:02:22 +0530

    Clerk setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,31 @@
+import { Show, SignInButton, SignOutButton, useUser } from "@clerk/react";
+
 const App = () => {
-  return <div>App</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen">
+      <Show when="signed-in">
+        <div className="flex justify-center items-center w-full flex-col gap-y-2">
+          <h1>{user?.id}</h1>
+          <h1>{user?.firstName}</h1>
+          <h1>{user?.lastName}</h1>
+          <h1>{user?.emailAddresses[0].emailAddress}</h1>
+          <img
+            src={user?.imageUrl}
+            alt="Pfp"
+            width={30}
+            height={30}
+            className="mb-6"
+          />
+          <SignOutButton>Logout</SignOutButton>
+        </div>
+      </Show>
+      <Show when="signed-out">
+        <SignInButton mode="modal">Login</SignInButton>
+      </Show>
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ClerkProvider>,
+);
