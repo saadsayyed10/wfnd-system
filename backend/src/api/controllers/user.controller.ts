9d4c03c6823114d80ccdd8f516fd3775dc1c88af
Saad Sayyed <saadsyed950@gmail.com>
2026-04-27 18:27:00 +0530

    Sync user controller

---
+++ backend/src/api/controllers/user.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { AppError } from "../../middleware/error.middleware";
+import { syncClerkUser } from "../services/user.service";
+
+export const syncClerkUserController = async (req: Request, res: Response) => {
+  const { clerkId, name, email, profilePicture, userType } = req.body;
+  const data = { clerkId, name, email, userType };
+
+  if (!data) {
+    console.log("Required fields are missing");
+    return res.status(404).json({ error: "Required fields are missing" });
+  }
+
+  try {
+    const user = await syncClerkUser(
+      clerkId,
+      name,
+      email,
+      profilePicture,
+      userType,
+    );
+
+    res.status(201).json({ message: "User synced", user });
+  } catch (error: any) {
+    console.log("Is AppError:", error instanceof AppError);
+    console.log("Error name:", error.constructor.name);
+    console.log("Error message:", error.message);
+    const status = error instanceof AppError ? error.statusCode : 500;
+    const message =
+      error instanceof AppError ? error.message : "Internal server error";
+    return res.status(status).json({ error: message });
+  }
+};
