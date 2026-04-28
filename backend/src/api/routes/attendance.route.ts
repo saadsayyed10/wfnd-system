3b88784ca6b97c3f4e11f60ffe69b2e20aed03be
Saad Sayyed <saadsyed950@gmail.com>
2026-04-28 15:45:15 +0530

    Create day route

--- backend/src/api/routes/attendance.route.ts
+++ backend/src/api/routes/attendance.route.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { createDayController } from "../controllers/attendance.controller";
 
 const attendanceRoutes = Router();
+
+attendanceRoutes.post("/create-day", createDayController);
 
 export default attendanceRoutes;
