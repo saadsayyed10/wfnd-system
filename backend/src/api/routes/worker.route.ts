0f2351f12523effd5264f40533ff439d4ec92ca3
Saad Sayyed <saadsyed950@gmail.com>
2026-04-28 11:32:46 +0530

    Worker routes

---
+++ backend/src/api/routes/worker.route.ts
@@ -0,0 +1,17 @@
+import { requireAuth } from "@clerk/express";
+import { Router } from "express";
+import {
+  addWorkerController,
+  deleteWorkerController,
+  fetchAllWorkersController,
+  updateWorkerController,
+} from "../controllers/worker.controller";
+
+const workerRoutes = Router();
+
+workerRoutes.post("/add", requireAuth(), addWorkerController);
+workerRoutes.get("/all", requireAuth(), fetchAllWorkersController);
+workerRoutes.put("/update", requireAuth(), updateWorkerController);
+workerRoutes.delete("/delete", requireAuth(), deleteWorkerController);
+
+export default workerRoutes;
