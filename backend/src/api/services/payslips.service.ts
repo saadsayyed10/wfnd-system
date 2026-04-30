60a829ffc4779f85191a1bed2c57e05375050b9a
Saad Sayyed <saadsyed950@gmail.com>
2026-04-30 16:59:33 +0530

    Working on payslip backend

---
+++ backend/src/api/services/payslips.service.ts
@@ -0,0 +1,7 @@
+export const generatePayslip = async () => {};
+
+export const fetchPayslips = async () => {};
+
+export const convertPayslipToExcel = async () => {};
+
+export const deletePayslip = async () => {};
