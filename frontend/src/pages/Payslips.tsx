a2adde90aebb75b3f7343d30150cd70e678e106d
Saad Sayyed <saadsyed950@gmail.com>
2026-05-01 16:13:39 +0530

    Delete payslip in frontend

--- frontend/src/pages/Payslips.tsx
+++ frontend/src/pages/Payslips.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/_components/Navbar";
-import { fetchPayslipAPI } from "@/api/payslip.api";
+import { deletePayslipAPI, fetchPayslipAPI } from "@/api/payslip.api";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -10,6 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Loader2 } from "lucide-react";
 import { useEffect, useState } from "react";
 
 interface PayslipData {
@@ -42,6 +43,9 @@ const typeMap: Record<string, string> = {
 
 const Payslip = () => {
   const [payslipData, setPayslipData] = useState<PayslipData[]>([]);
+
+  const [loading, setLoading] = useState(false);
+
   const weekStart = "2026-04-27T10:39:40.359Z";
   const weekEnd = "2026-04-30T10:39:40.359Z";
 
@@ -55,6 +59,19 @@ const Payslip = () => {
     }
   };
 
+  const handleDeletePayslips = async () => {
+    setLoading(true);
+    try {
+      await deletePayslipAPI();
+      alert("Payslip deleted");
+    } catch (error: any) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+      fetchPayslipData();
+    }
+  };
+
   useEffect(() => {
     fetchPayslipData();
   }, []);
@@ -64,28 +81,35 @@ const Payslip = () => {
       <Navbar />
       <div className="flex justify-center min-h-screen items-center w-full flex-col lg:gap-y-8 gap-y-4 lg:p-10 p-6">
         <div className="flex justify-between items-center w-full">
-          {/* <div className="flex justify-start items-center w-full lg:gap-x-2 gap-x-1">
-            <Search className="lg:w-6 lg:h-6 w-4 h-4 opacity-25" />
-            <Input className="w-62" placeholder="Search worker..." />
-          </div> */}
-          <div />
+          <Button variant="secondary">Export to .xlsx</Button>
           <div className="flex justify-end items-end w-full flex-row gap-x-2">
             <Button>Generate Payslip</Button>
-            <Button variant="secondary">Delete</Button>
+            <Button variant="destructive" onClick={handleDeletePayslips}>
+              {loading ? (
+                <Loader2 className="w-4 h-4 animate-spin" />
+              ) : (
+                "Delete"
+              )}
+            </Button>
           </div>
         </div>
         <Table>
-          <TableCaption>
-            Wage statement for period: 20-04-2026 to 25-04-2026
-          </TableCaption>
+          {payslipData.length !== 0 && (
+            <TableCaption>
+              Wage statement for period: 20-04-2026 to 25-04-2026
+            </TableCaption>
+          )}
           <TableHeader className="border">
             <TableRow>
               <TableHead>Sr No.</TableHead>
               <TableHead>Worker</TableHead>
+              <TableHead>SUN</TableHead>
               <TableHead>MON</TableHead>
               <TableHead>TUE</TableHead>
               <TableHead>WED</TableHead>
               <TableHead>THU</TableHead>
+              <TableHead>FRI</TableHead>
+              <TableHead>SAT</TableHead>
               <TableHead>O/T Hours</TableHead>
               <TableHead>Total Days</TableHead>
               <TableHead>Actual Day</TableHead>
@@ -94,20 +118,33 @@ const Payslip = () => {
             </TableRow>
           </TableHeader>
           <TableBody className="border">
-            {payslipData.map((payslip, idx) => (
-              <TableRow key={payslip.id}>
-                <TableCell>{idx + 1}</TableCell>
-                <TableCell>{payslip.workers.name}</TableCell>
-                {payslip.payslip_data.map((pD, idx) => (
-                  <TableCell key={idx}>{typeMap[pD.type] ?? pD.type}</TableCell>
-                ))}
-                <TableCell>{payslip.overtime_total}</TableCell>
-                <TableCell>{payslip.total_days}</TableCell>
-                <TableCell>{payslip.actual_days}</TableCell>
-                <TableCell>{payslip.workers.daily_payment}</TableCell>
-                <TableCell>{payslip.weekly_wage}</TableCell>
+            {payslipData.length === 0 ? (
+              <TableRow>
+                <TableCell
+                  colSpan={14}
+                  className="text-center align-middle lg:py-6"
+                >
+                  Please generate payslip of this week to view
+                </TableCell>
               </TableRow>
-            ))}
+            ) : (
+              payslipData.map((payslip, idx) => (
+                <TableRow key={payslip.id}>
+                  <TableCell>{idx + 1}</TableCell>
+                  <TableCell>{payslip.workers.name}</TableCell>
+                  {payslip.payslip_data.map((pD, idx) => (
+                    <TableCell key={idx}>
+                      {typeMap[pD.type] ?? pD.type}
+                    </TableCell>
+                  ))}
+                  <TableCell>{payslip.overtime_total}</TableCell>
+                  <TableCell>{payslip.total_days}</TableCell>
+                  <TableCell>{payslip.actual_days}</TableCell>
+                  <TableCell>{payslip.workers.daily_payment}</TableCell>
+                  <TableCell>{payslip.weekly_wage}</TableCell>
+                </TableRow>
+              ))
+            )}
           </TableBody>
         </Table>
       </div>
