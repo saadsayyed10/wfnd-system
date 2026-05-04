8d66aa9a2fd2382aeec0d520834f703eb2c7c05b
Saad Sayyed <saadsyed950@gmail.com>
2026-05-04 13:20:30 +0530

    Mobile responsive navbar sheet

--- frontend/src/_components/Navbar.tsx
+++ frontend/src/_components/Navbar.tsx
@@ -1,3 +1,10 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { useApproval } from "@/hooks/useApproval";
 import { UserButton } from "@clerk/react";
 import { Menu } from "lucide-react";
@@ -18,7 +25,32 @@ const Navbar = () => {
       </div>
       <div className="flex justify-end items-center w-full flex-row gap-x-4">
         <UserButton />
-        <Menu className="w-4 h-4 block lg:hidden" color="white" />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Menu className="w-4 h-4 block lg:hidden" color="white" />
+          </SheetTrigger>
+          <SheetContent className="bg-black">
+            <SheetHeader>
+              <SheetTitle className="text-white text-2xl mt-4 font-semibold">
+                WFND Menu
+              </SheetTitle>
+            </SheetHeader>
+            <ul className="flex justify-start items-start w-full flex-col gap-y-4 text-lg text-white p-4">
+              <li>
+                <Link to={"/"}>1. Dashboard</Link>
+              </li>
+              <li>
+                <Link to={"/attendance"}>2. Attendance</Link>
+              </li>
+              <li>
+                <Link to={"/workers"}>3. Workers</Link>
+              </li>
+              <li>
+                <Link to={"/payslips"}>4. Pay slips</Link>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   ) : (
