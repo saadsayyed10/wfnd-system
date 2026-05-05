c268a9a657289cb1c815da7b283f04f61d8009c6
Saad Sayyed <saadsyed950@gmail.com>
2026-05-05 17:22:58 +0530

    Trying to get backend run in production

--- frontend/src/api/apiUrl.ts
+++ frontend/src/api/apiUrl.ts
@@ -1,4 +1,6 @@
-const apiUrl = "https://wfnd-system-production.up.railway.app/api";
-// const apiUrl = "http://localhost:3001/api";
+const apiUrl = "http://localhost:3001/api";
+
+// const apiUrl = "https://wfnd-system-production.up.railway.app/api";
+// const apiUrl = import.meta.env.API_URL!;
 
 export default apiUrl;
