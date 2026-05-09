c624ce24c20416e49902aa1a689ef7068a277bd2
Saad Sayyed <saadsyed950@gmail.com>
2026-05-09 11:49:28 +0530

    Deploying payslip changes

--- frontend/src/api/apiUrl.ts
+++ frontend/src/api/apiUrl.ts
@@ -1,4 +1,4 @@
-const apiUrl = "http://localhost:3001/api";
-// const apiUrl = "https://wfnd-system.up.railway.app/api";
+// const apiUrl = "http://localhost:3001/api";
+const apiUrl = "https://wfnd-system.up.railway.app/api";
 
 export default apiUrl;
