395708a273ed3a13fdc9156e4fe08824174a2010
Saad Sayyed <saadsyed950@gmail.com>
2026-05-11 17:23:57 +0530

    Add worker notification

--- backend/src/api/services/notification.service.ts
+++ backend/src/api/services/notification.service.ts
@@ -1,17 +1,5 @@
 import prisma from "../../lib/prisma.orm";
 
-export const createNotificationService = async (
-  title: string,
-  description: string,
-) => {
-  return await prisma.notifications.create({
-    data: {
-      title,
-      description,
-    },
-  });
-};
-
 export const fetchAllNotifications = async () => {
   return await prisma.notifications.findMany();
 };
--- backend/src/api/services/worker.service.ts
+++ backend/src/api/services/worker.service.ts
@@ -1,12 +1,20 @@
 import prisma from "../../lib/prisma.orm";
+import { triggerNotification } from "../../lib/trigger-notification";
 
 export const addWorker = async (name: string, dailyPayment: number) => {
-  return await prisma.workers.create({
+  const worker = await prisma.workers.create({
     data: {
       name: name,
       daily_payment: dailyPayment,
     },
   });
+
+  await triggerNotification(
+    "Worker",
+    `New worker ${name} is added to the system with the daily payment of ₹ ${dailyPayment}`,
+  );
+
+  return worker;
 };
 
 export const fetchAllWorkers = async () => {
