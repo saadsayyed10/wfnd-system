666b2d4b4fca6c6061f2eba7b44d50c669e9a9a1
Saad Sayyed <saadsyed950@gmail.com>
2026-05-11 14:53:31 +0530

    Mobile navbar overhaul

--- frontend/src/_components/Navbar.tsx
+++ frontend/src/_components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   const { approval } = useApproval();
 
   return approval ? (
-    <div className="flex justify-between items-center w-full lg:px-12 px-6 bg-black text-white absolute top-0 flex-row">
+    <div className="lg:flex hidden justify-between items-center w-full lg:px-12 px-6 bg-black text-white absolute top-0 flex-row">
       {/* <h1 className="lg:text-4xl font-bold">WFND</h1> */}
       <img src="./logo.png" width={110} height={80} />
       <div className="lg:flex hidden justify-center items-center w-full gap-x-12 font-medium text-sm tracking-normal">
--- frontend/src/api/apiUrl.ts
+++ frontend/src/api/apiUrl.ts
@@ -1,4 +1,3 @@
-const apiUrl = "http://localhost:3001/api";
-// const apiUrl = "https://wfnd-system.up.railway.app/api";
+const apiUrl = import.meta.env.PROD_VITE_API_URL || "http://localhost:3001/api";
 
 export default apiUrl;
